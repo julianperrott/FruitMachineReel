612254241613575b9e8b4696cf93a3673fd75d00
Julian Perrott <julian_perrott@hotmail.com>
2017-07-16 21:56:43 +0100

    Update Readme

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-"# FruitMachineReel" 
+## Rendering a Fruit Machine Reel
+
+So, this is a bit of experimentation, playing with the position and size of images to see if I can create an animated fruit machine reel.
+
+I had the idea of showing the result of a random choice via an animation on my site homepage. I didn't want to use a canvas for this, so I wondered if I could use image tags. The first idea of how I could represent this random result was a fruit machine reel, because it also shows that there are different options, only one of which is chosen.
+![Fruit Machine Reels](images/reels.jpg "Fruit Machine Reels")
+
+A fruit machine reel is circular with only about 50% visible when viewed side on. The diagram below shows that the segments are of equal size but depending upon their position can appear to have different heights when viewed by the user. By plotting the XY of the circumference of the circle we can work out the Y co-ordinate and determine the height of each segment depending on the number of degrees it covers from it's start position to it's end.
+
+<pre class="prettyprint" >
+for (var i = 0; i < 180; i += 1) {
+    var x = radius * Math.sin(2 * Math.PI * (i - 90) / 360);
+    var y = radius * Math.cos(2 * Math.PI * (i - 90) / 360);
+    ...
+}
+</pre>
+
+![A diagram of a circle segments showing the height of each segment and images next to it showing the height of the image](images/FruitMachineReel.jpg "A diagram of a circle segments showing the height of each segment and images next to it showing the height of the image")
+
+## A Demo
+
+You can see what it looks like moving in an implemention on plunker. The animated reel uses AngularJs to do the animation.
+
+[Link to plunker](https://run.plnkr.co/plunks/p2cn15/)
+
+![Demo Screenshot](images/FruitMachinePlunkerLink.png "Demo Screenshot")
+
+It works roughly like this:
+
+* First the y coordinates on one side of a circle are calculated.
+* A list of images objects is defined, each one has a reference to a PNG image.
+* The position in degrees of one of the images is tracked.
+* A timer fires a few times a second and steps the tracked image a set number of degrees down.
+* The position of all of the other images is calculated from the one we know.
+* Once the image has passed the 90 degree angle the tracked image moves to the next in the list.
+* The images are shown in an html div in their calaculated position, by setting the CSS height and absolute position from the top.
+
+The result is a reasonably convincing representation of a fruit machine reel. It runs faster in Chrome compared to Internet Explorer. It can be a little jerky so it is not perfect. In a real fruit machine you can't see the whole side of the reel, so it that respect it is less realistic or useful.
+
+
+
+
+[Link to plunker](https://run.plnkr.co/plunks/hHpbWM/)
+![Demo Screenshot 2](images/ImageInfo.png "")
--- script.js
+++ script.js
@@ -113,3 +113,169 @@ app.controller('fruitMachine', ['$interval', function ($interval) {
     
     vm.start();
 }]);
+
+
+app.controller('fruitMachine2', ['$interval', function ($interval) {
+    var vm = this;
+    vm.start = function () { 
+      vm.stop();
+      vm.interval = $interval(vm.move, 50); 
+    };
+    
+    vm.stop = function () { 
+      if (vm.interval) {
+        $interval.cancel(vm.interval); 
+      }
+    };
+
+    vm.move = function () {
+        vm.reelViews = vm.reels.map(function (reel, i) {
+            reel.move();
+            var views = vm.reelView.createSymbolViews(reel.degrees, reel.symbolSize);
+            views.forEach(function (v) { v.symbolNo = (v.symbolNo + reel.symbolsCount + reel.symbolNo) % reel.symbolsCount });
+            views.index = i;
+            return views;
+        });
+    };
+
+    vm.createReel = function (symbols, degreesStep, symbolSize) {
+        return {
+            symbolsCount: symbols.length,
+            degreesStep: degreesStep,
+            symbolNo: 0,
+            degrees: 90,
+            symbolSize: symbolSize,
+            move: function () {
+                var c = document.getElementById("myCanvas");
+                var ctx = c.getContext("2d");
+                ctx.strokeStyle = '#000000';
+                ctx.clearRect(0, 0, 600,600);
+                
+                ctx.beginPath();
+                ctx.arc(0,300,300,0,2*Math.PI);
+                ctx.stroke();
+                
+                var c2 = document.getElementById("myCanvas2");
+                var ctx2 = c2.getContext("2d");
+                ctx2.strokeStyle = '#000000';
+                ctx2.clearRect(0, 0, 600,600);
+              
+                this.degrees += this.degreesStep; // move the required number of degrees
+
+                if (this.degrees > 90) { // 90 is the centre of the reel
+                    this.symbolNo = (this.symbolNo + 1) % this.symbolsCount;
+                    this.degrees = this.degrees - this.symbolSize; // move the reel location
+                }
+            }
+        };
+    };
+
+    vm.createReelViewGenerator = function (height) {
+
+        var radius = height / 2;
+        var reelViewGenerator = {
+            degreeOffsety: [],
+            degreeOffsetx: [],
+
+            createSymbolViews: function (degrees, symbolSize) {
+                var symbolNo = 0;
+
+                // move to top of reel
+                while (degrees >= 0) {
+                    degrees -= symbolSize;
+                    symbolNo++;
+                }
+
+                // now move down to fill the reel wth symbols
+                var views = [];
+                while (degrees < 180) {
+                    views.push(this.createSymbolView(symbolSize, degrees, symbolNo));
+                    degrees += symbolSize;
+                    symbolNo--;
+                }
+                return views;
+            },
+
+            createSymbolView: function (symbolHeight, degreesStart, symbolNo) {
+                // works out where each symbol starts and ends vertically so that it can be scaled to that height
+                var view = { scale: 1, symbolNo: symbolNo, imgTop: 0 };
+
+                var degreesEnd = degreesStart + symbolHeight - 1;
+                if (degreesStart < 0) { degreesStart = 0; }
+                if (degreesEnd >= 180) { degreesEnd = 179; }
+                if (degreesEnd < 0) { degreesEnd = 0; }
+
+                view.startPx = this.degreeOffsety[degreesStart];
+                view.startPy = this.degreeOffsetx[degreesStart];
+                view.endPx = this.degreeOffsety[degreesEnd];
+                view.height = (view.endPx - view.startPx);
+                view.heightdiv = view.height;
+
+                if (degreesEnd - degreesStart < symbolHeight - 1 && degreesStart < 179 && degreesEnd > 0) {
+                    view.scale = (degreesEnd - degreesStart) / (symbolHeight-1);
+                    view.height = Math.round(view.height / view.scale) + 0;
+                    if (degreesStart === 0) {
+                        view.imgTop = (view.endPx - view.startPx) - view.height;
+                    }
+                }
+                
+                var c = document.getElementById("myCanvas");
+                var ctx = c.getContext("2d");
+                ctx.beginPath();
+                ctx.moveTo(0,300);
+                ctx.strokeStyle = '#000000';
+                ctx.lineTo(view.startPy-300,view.startPx);
+                ctx.stroke();
+                
+                ctx.closePath();
+                
+                ctx.beginPath();
+                ctx.setLineDash([5, 3]);
+                ctx.strokeStyle = 'orange';
+                ctx.moveTo(view.startPy-298,view.startPx);
+                ctx.lineTo(302,view.startPx);
+                ctx.stroke();
+                
+              ctx.closePath();
+              
+               c = document.getElementById("myCanvas2");
+              ctx = c.getContext("2d");
+                ctx.beginPath();
+                ctx.setLineDash([1, 2]);
+                ctx.strokeStyle = 'white';
+                ctx.moveTo(132,view.startPx+view.height/2);
+                ctx.lineTo(180,13+view.startPx+view.height/4);
+                ctx.stroke();
+                
+              ctx.closePath();
+
+                return view;
+            },
+        };
+
+        // the vertical position of each degree from -90 to + 90 (the visible face of the reel)
+        for (var i = 0; i < 180; i += 1) {
+            var y = radius * Math.sin(2 * Math.PI * (i - 90) / 360);
+            reelViewGenerator.degreeOffsety[i] = Math.round(y + radius);
+            
+            var x = radius * Math.cos(2 * Math.PI * (i - 90) / 360);
+            reelViewGenerator.degreeOffsetx[i] = Math.round(x + radius);
+        }
+
+        return reelViewGenerator;
+    };
+
+    // initialise
+    vm.symbols = ["images/1.png", "images/2.png", "images/3.png", "images/4.png", "images/5.png", "images/6.png", "images/7.png", "images/8.png", "images/9.png"];
+    vm.height=600;
+    vm.reelView = vm.createReelViewGenerator(vm.height);
+    vm.reelcount = 1;
+
+    vm.reels = [];
+    for (var i = 0; i < vm.reelcount; i++) {
+        vm.reels.push(vm.createReel(vm.symbols, 1, 30));
+        vm.reels[i].symbolNo = Math.round(Math.random() * vm.symbols.length); // random start symbol
+    }
+    
+    vm.start();
+}]);
